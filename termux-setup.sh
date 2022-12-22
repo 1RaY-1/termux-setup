d97b8889319a2ee5172df19994cdef84459aea80
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2022-12-22 12:11:53 +0100

    improve a little

--- termux-setup.sh
+++ termux-setup.sh
@@ -1,15 +1,18 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# My Termux setup
+# download and run this script by enetering this in your terminal (without hashtag):
+# pkg update && apt install git -y
+# git clone https://github.com/1RaY-1/termux-setup && cd termux-setup && chmod +x termux-setup.sh && bash termux-setup.sh
 
-# update, upgrade
-pkg update && pkg upgrade -y
+
+# upgrade
+pkg upgrade -y
 
 # configure storage
 termux-setup-storage
 
 # INSTALL SOME PROGRAMS
-for each_pac in "wget" "net-tools" "mlocate" "python3" "clang" "zip" "unzip" "tree" "git" "cmatrix" "htop" "grep" "vim" "neofetch"; do
+for each_pac in "wget" "net-tools" "mlocate" "python3" "clang" "zip" "unzip" "tree" "git" "cmatrix" "htop" "grep" "vim" "neofetch" "curl"; do
     pkg install $each_pac -y
 done
 
@@ -21,23 +24,18 @@ done
 # confifure vim
 # install monokai color scheme 
 git clone https://github.com/sickill/vim-monokai 
-
 mkdir -p ~/.vim/colors
-
 mv vim-monokai/colors/monokai.vim ~/.vim/colors && rm -rf vim-monokai/
 
 # configure vim config file (you can edit it, by the way)
 cat > ~/.vimrc << EOF
 syntax enable
 colorscheme monokai
-
 " enable mouse support
 set mouse=a
-
 " set black background
 hi Normal ctermbg=16 guibg=#000000
 hi LineNr ctermbg=16 guibg=#000000
-
 set cursorline
 hi cursorline gui=underline cterm=underline ctermbg=NONE
 set smartindent
@@ -51,16 +49,28 @@ inoremap { {}<left>
 inoremap " ""<left>
 EOF
 
-# upgrade pip and install some modules
-pip install --upgrade pip
-pip install setuptools
-pip install requests
+# install some python modules
+for each in "setuptools" "requests" "ipython" "cryptography" "faker"
+    pip3 install $each
+done
+
+# Install sms b0mb3r (https://github.com/Denishnc/b0mb3r)
+cd ~
+git clone https://github.com/Denishnc/b0mb3r
+
+# Install ip tracer (https://github.com/rajkumardusad/IP-Tracer.git)
+cd ~
+git clone https://github.com/rajkumardusad/IP-Tracer.git
+cd IP-Tracer
+chmod +x install
+./install
 
 # MORE
 # remove Termux welcome text
 rm /data/data/com.termux/files/usr/etc/motd
 
 
+# FINISH
 clear
 echo "Termux setup complete!"
 exit 0
